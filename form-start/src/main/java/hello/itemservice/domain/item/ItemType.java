bca3e4d750aca1eb4e82d707190204e3ca44eddf
wonsakku <kws1044@gmail.com>
2021-11-13 18:20:31 +0900

    project init

---
+++ form-start/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ItemType {
+
+	BOOK("도서"), FOOD("음식"), ETC("기타");
+	private final String description;
+}
